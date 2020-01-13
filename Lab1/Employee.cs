1a407b02153ff53652cf12429dd4964980646a92
CHIRAG M BARANDA <45106856+chiragmb96@users.noreply.github.com>
2020-01-13 15:53:28 -0500

    Add files via upload

---
+++ Lab1/Employee.cs
@@ -0,0 +1,150 @@
+﻿///
+/// Program: Employee Class
+/// 
+/// Author: Chirag Baranda
+/// 000759867
+/// Date:   23 November 2018
+/// I, Chirag Baranda, 000759867 certify that this material is my original work. No other person's work has been used without due acknowledgement.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// This class describes the employee
+/// </summary>
+public class Employee : IComparable<Employee>
+{
+  
+
+    /// <summary>
+    /// get and Set Properties for The employee name
+    /// </summary>
+    /// <returns> The employee name </returns>
+    public string Name{ get; set; }
+
+    /// <summary>
+    /// get and Set Properties for employee ID
+    /// </summary>
+    /// <returns> Employee ID </returns>
+    public int Number { get; set; }
+
+    /// <summary>
+    /// get and Set Properties for hours
+    /// </summary>
+    /// <returns> Employee ID </returns>
+    public Double Hours { get; set; }
+
+
+    /// <summary>
+    /// get and Set Properties for Hourly rate
+    /// </summary>
+    /// <returns>Hourly rate of the week</returns>
+    public decimal Rate { get; set; }
+    /// <summary>
+    /// get and Set Properties for Hours 
+    /// </summary>
+    /// <returns>Hours of the week</returns>
+    
+
+    public decimal gross;
+
+    /// <summary>
+    /// Gross pay get ans Set Property
+    /// </summary>
+    /// <returns>calculated Gross pay for the week</returns>
+    public decimal Gross { get { return Gross; } set {  this.Gross = (Hours < 40) ? (decimal)Hours * Rate : (40.0m * Rate) + (((decimal)Hours - 40.0m) * Rate * 1.5m); } }
+
+    /// <summary>
+    /// Default constructor for Employee - used for creating Employee array
+    /// </summary>
+    public Employee()
+  {
+  }
+
+  /// <summary>
+  /// Four-argument constructor for Employee
+  /// </summary>
+  /// <param name="name">Employee name</param>
+  /// <param name="number">Employee number</param>
+  /// <param name="rate">Hourly rate of pay</param>
+  /// <param name="hours">Hours worked in a week</param>
+  public Employee(string name, int number, decimal rate, double hours)
+  {
+        /* This is the better way to set data in a class - use the accessor methods.
+         * That way, if their implementation changes, the constructor doesn't need to
+         * be edited as well.
+         */
+        this.Name = name;
+        this.Number = number;
+        this.Rate = rate;
+        this.Hours = hours;
+        this.Gross = Gross;
+  }
+
+  
+
+  /// <summary>
+  /// Employee display method - in the future, we'll override the ToString method of Object
+  /// </summary>
+  public override String ToString() => $"{Name,-20}  {Number:D5}  {Rate,6:C}  {Hours:#0.00}  {Gross,9:C}";
+
+    public int CompareTo(Employee other , EmployeeDataComparer.ComparisonType which)
+    {
+        switch(which)
+        {
+            case EmployeeDataComparer.ComparisonType.Name: return this.Name.CompareTo(other.Name);
+            case EmployeeDataComparer.ComparisonType.Number: return this.Number.CompareTo(other.Number);
+            case EmployeeDataComparer.ComparisonType.Hours: return this.Hours.CompareTo(other.Hours);
+            case EmployeeDataComparer.ComparisonType.Rate: return this.Rate.CompareTo(other.Rate);
+            case EmployeeDataComparer.ComparisonType.Gross: return this.Gross.CompareTo(other.Gross);
+        }
+        return 0;
+    }
+
+    public static EmployeeDataComparer GetDataComparer()
+    {
+        return new EmployeeDataComparer();
+    }
+
+    public int CompareTo(Employee other)
+    {
+        return this.Name.CompareTo(other.Name);
+    }
+
+
+
+
+    public class EmployeeDataComparer : IComparer<Employee>
+    {   /// <summary>
+        /// 
+        /// </summary>
+        public EmployeeDataComparer.ComparisonType WhichComparison
+        { get { return WhichComparison; } set { this.WhichComparison = WhichComparison; } }
+        /// <summary>
+        /// Creating types using Enumarator
+        /// which will be used as a type for comparison
+        /// </summary>
+        public enum ComparisonType
+        {
+            Name,
+            Number,
+            Rate,
+            Hours,
+            Gross
+        };
+        /// <summary>
+        /// Compare method for comapring two Employee object
+        /// </summary>
+        /// <param name="empData1"></param>
+        /// <param name="empData2"></param>
+        /// <returns>0 - If less than 0, x is less than y | -If 0, x equals y | -if greater than 0, x is greater than y.</returns>
+        public int Compare(Employee empData1, Employee empData2)
+        {
+            return empData1.CompareTo(empData2, WhichComparison);
+        }
+
+    }  
+}
---
+++ Lab1/Lab4A.cs
@@ -0,0 +1,216 @@
+﻿///
+/// Program: Employee Sort
+/// 
+/// Author: Chirag Baranda
+/// 000759867
+/// Date:   23 November 2018
+/// I, Chirag Baranda, 000759867 certify that this material is my original work. No other person's work has been used without due acknowledgement.
+/// Purpose:  This program reads the employee.txt data file and stores the information in an array of
+///           Employee objects.  The user is then presented with a menu that allows them to select which
+///           of five fields to sort the resultant table by: Employee name, ID, rate, hours, or gross pay.
+///           
+///           The program ends when the user selects the exit option from the menu.
+///
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+/// <summary>
+/// This is the main tester class that the program starts at
+/// </summary>
+class Lab1
+{
+    /// <summary>
+    /// The main method reads the data file, populates the Employee array and provides a menu of sort options.
+    /// </summary>
+    /// <param name="args">Command line arguments are not used in this program</param>
+    static void Main(string[] args)
+    {
+                                              // Keep track of how many employees are instantiated
+        bool loop = true;                               // A loop control variable
+        string input;                                   // The user's menu option pick as a string
+        int choice;                                     // The user's menu option pick as an integer
+        //Employee[] employees;                           // The array of Employee objects
+        List<Employee> employees = new List<Employee>();
+        // Read the data file to build the Employee array and find out how many there are
+        int count = Read(employees);
+
+        // Keep the menu loop running so the user can sort several times
+        while (loop)
+        {
+            // Display the menu and retrieve the user's choice
+            input = Menu();
+
+            // Based on the user's entry, sort using the appropriate option
+            if (Int32.TryParse(input, out choice))
+            {
+                switch (choice)
+                {
+                    // Sort by employee name - ascending
+                    case 1:
+                        Sort(employees, count, 1);
+                        break;
+
+                    // Sort by employee ID number - ascending
+                    case 2:
+                        Sort(employees, count, 2);
+                        break;
+
+                    // Sort by hourly rate - descending
+                    case 3:
+                        Sort(employees, count, 3);
+                        break;
+
+                    // Sort by weekly hours - descending
+                    case 4:
+                        Sort(employees, count, 4);
+                        break;
+
+                    // Sort by gross pay - descending
+                    case 5:
+                        Sort(employees, count, 5);
+                        break;
+
+                    // Exit the program
+                    case 6:
+                        loop = false;
+                        break;
+
+                    // Trap invalid selections to try again
+                    default:
+                        Console.WriteLine("\n*** Invalid Choice - Try Again ***\n");
+                        break;
+                }
+
+                // Display the table when a valid choice is made, otherwise display an error
+                if (choice >= 0 && choice <= 5)
+                    DisplayTable(employees, count);
+            }
+            else
+                Console.WriteLine("\n*** Invalid Choice - Try Again ***\n");
+        }
+    }
+
+    /// <summary>
+    /// This method displays the entire table, including column headers
+    /// </summary>
+    /// <param name="employees">The array of employees</param>
+    /// <param name="count">How many employees are in use</param>
+    private static void DisplayTable(List<Employee> employees, int count)
+    {
+        Console.Clear();
+        Console.WriteLine("Employee              Number    Rate  Hours  Gross Pay           Nick's Company");
+        Console.WriteLine("====================  ======  ======  =====  =========           --------------");
+
+        // Display each employee in the array
+        for (int i = 0; i < count; i++)
+          Console.WriteLine(employees[i]); 
+        Console.WriteLine();
+    }
+
+    /// <summary>
+    /// This method displays the menu options to the user and returns their selection
+    /// </summary>
+    /// <returns>The user's menu selection</returns>
+    private static string Menu()
+    {
+        Console.WriteLine("1. Sort by Employee Name");
+        Console.WriteLine("2. Sort by Employee Number");
+        Console.WriteLine("3. Sort by Employee Pay Rate");
+        Console.WriteLine("4. Sort by Employee Hours");
+        Console.WriteLine("5. Sort by Employee Gross Pay");
+        Console.WriteLine("\n6. Exit");
+        Console.Write("\nEnter choice: ");
+
+        return Console.ReadLine();
+    }
+
+    /// <summary>
+    /// This method reads the data file and stores all of the employee information in an array of Employees
+    /// </summary>
+    /// <param name="employees">The array of employees</param>
+    /// <param name="count">How many employees are in use</param>
+    private static int Read(List<Employee> employees)
+    {
+        int count = 0;                                    // The current number of employees
+        string input;                                 // One line of data read from the file
+        employees = new List<Employee>(100);                // The Employee array
+
+
+        try
+        {
+            // Open the file for reading purposes
+            FileStream file = new FileStream("employees.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(file);
+            
+            // As long as there is data in the file, keep processing 
+            // Each employee record is comma separated, so explode each piece into an array,
+            // create a new employee object and increment the count
+            while ((input = reader.ReadLine()) != null)
+            {
+                string[] exploded = input.Split(',');
+                employees.Add(new Employee(exploded[0], int.Parse(exploded[1]), decimal.Parse(exploded[2]), double.Parse(exploded[3])));
+                count++;
+            }
+
+            reader.Close();                             // Always good form to close the file
+        }
+
+        // Just in case the file can't be found - graceful exit
+        catch (IOException e)
+        {
+            Console.WriteLine("*** File is empty - Program Aborting ***\n");
+            Environment.Exit(1);
+        }
+        return count;
+    }
+
+    /// <summary>
+    /// This method uses a Bubble Sort to rearrange the Employee array in order.  Since there are 
+    /// five possible ways to sort, the outer loops remain the same and instead the condition is
+    /// different for each field to sort on.
+    /// </summary>
+    /// <param name="emps">The array of employees</param>
+    /// <param name="size">How many employees are in use</param>
+    /// <param name="choice">The user selected sort choice</param>
+    public static void Sort(List<Employee> employees, int count, int choice)
+    {
+
+        Employee.EmployeeDataComparer e = Employee.GetDataComparer();
+        switch (choice)
+        {
+            
+            case 1:
+                e.WhichComparison = Employee.EmployeeDataComparer.ComparisonType.Name;
+                employees.Sort(e);
+                break;
+            
+            case 2:
+                e.WhichComparison = Employee.EmployeeDataComparer.ComparisonType.Number;
+                employees.Sort(e);
+                break;
+            
+            case 3:
+                e.WhichComparison = Employee.EmployeeDataComparer.ComparisonType.Rate;
+                employees.Sort(e);
+                break;
+            
+            case 4:
+                e.WhichComparison = Employee.EmployeeDataComparer.ComparisonType.Hours;
+                employees.Sort(e);
+                break;
+            
+            case 5:
+                e.WhichComparison = Employee.EmployeeDataComparer.ComparisonType.Gross;
+                employees.Sort(e);
+                break;
+            default:
+                break;
+
+        }
+
+    }
+
+
+}
+
